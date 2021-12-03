6c36176ee41743523504d2cdbfe367ea4c19058a
hmimusc <hbillah181290@bscse.uiu.ac.bd>
2021-12-03 22:28:20 +0600

    preparing index page for general_app

--- app_general/urls.py
+++ app_general/urls.py
@@ -5,4 +5,6 @@ app_name = "app_general"
 
 urlpatterns = [
     path("index/", views.Index.as_view(), name="index"),
+    path("student_registration_page/", views.StudentRegistrationPage.as_view(), name="student_registration_page"),
+    path("login_page/", views.LoginPage.as_view(), name="login_page"),
 ]
--- app_general/views.py
+++ app_general/views.py
@@ -11,3 +11,16 @@ class Index(TemplateView):
     def get_context_data(self, *args, **kwargs):
         return super().get_context_data(*args, **kwargs)
 
+class StudentRegistrationPage(TemplateView):
+
+    template_name = "app_general/student_registration.html"
+
+    def get_context_data(self, *args, **kwargs):
+        return super().get_context_data(*args, **kwargs)
+
+class LoginPage(TemplateView):
+
+    template_name = "app_general/login_page.html"
+
+    def get_context_data(self, *args, **kwargs):
+        return super().get_context_data(*args, **kwargs)
