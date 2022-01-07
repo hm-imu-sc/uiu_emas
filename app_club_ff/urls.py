5dd7ab412960ddb6b707ee716f54b695e4a8b012
Md. Tarek Hasan <mhasan181076@bscse.uiu.ac.bd>
2022-01-07 20:17:15 +0600

    Student Information fetched from DB to show in Fest Registration Page

--- app_club_ff/urls.py
+++ app_club_ff/urls.py
@@ -2,8 +2,18 @@ from django.conf import settings
 from django.urls import path
 from . import views
 
+from django.shortcuts import render
+from my_modules.base_views import DBAction, DBRead
+from django.views.generic import TemplateView
+from my_modules.database import MySql
+from django.http import HttpResponse
+import json
+
 app_name = "app_club_ff"
 
 urlpatterns = [
-    path("index/", views.Index.as_view(), name="index")
+    path("index/", views.Index.as_view(), name="index"),
+    path("fest_registration_page/<student_id>",views.FestRegistrationPage.as_view(), name="fest_registration_page"),
+
+
 ]
--- app_club_ff/views.py
+++ app_club_ff/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from my_modules.base_views import DBAction
 from django.views.generic import TemplateView
 
+from django.shortcuts import render
+from my_modules.base_views import DBAction, DBRead
+from django.views.generic import TemplateView
+from my_modules.database import MySql
+from django.http import HttpResponse
+import json
+
+
 # Create your views here.
 
 class Index(TemplateView):
@@ -9,3 +17,18 @@ class Index(TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         return super().get_context_data(*args, **kwargs)
+
+class FestRegistrationPage(TemplateView):
+    template_name = "app_club_ff/fest_registration_page.html"
+    database = MySql.db()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        student_id = kwargs["student_id"]
+
+        student = self.database.query(f"SELECT student_id, name, uiu_email FROM students WHERE student_id = '{student_id}'")
+        context["data"] = []
+        list = ['student_id', 'name', 'uiu_email']
+        for tup in student:
+            context["data"].append({list[0]:tup[0],list[1]:tup[1], list[2]: tup[2]})
+        return context
