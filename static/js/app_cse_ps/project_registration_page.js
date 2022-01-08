a8fcda20afcb0db4eda0a95e0a5f432517f869b6
hm-imu-sc <hmimusc850@gmail.com>
2022-01-08 13:41:21 +0600

    some minor UI changes applied: navbar, project_registration_page

--- static/js/app_cse_ps/project_registration_page.js
+++ static/js/app_cse_ps/project_registration_page.js
@@ -1,104 +1,110 @@
 $("#course_name").click(function()
 {
-    let option = $("#course_name option");
-    if(option.length>1)
-    {
-      return;
-    }
-    $.ajax({
-      url: "/cse_ps/get_course_names/",
-      method: "GET",
-      success: function(data)
-      {
-        data = JSON.parse(data);
+	let option = $("#course_name option");
+	if(option.length>1)
+	{
+		return;
+	}
+	$.ajax({
+		url: "/cse_ps/get_course_names/",
+		method: "GET",
+		success: function(data)
+		{
+			data = JSON.parse(data);
 
-        let html_for_option = "<option value=''>Select Course</option> ";
-        data = data['data'];
+			let html_for_option = "<option value=''>Select Course</option> ";
+			data = data['data'];
 
-        for(let i=0;i<data.length;i++)
-        {
-          html_for_option+=("<option value = '" + data[i]['course_code'] + "'>" + data[i]['course_code'].replace('_', ' ') + " : " + data[i]['course_name'] + "</option> ");
-        }
-        $("#course_name").html(html_for_option);
-      }
-    });
+			for(let i=0;i<data.length;i++)
+			{
+				html_for_option+=("<option value = '" + data[i]['course_code'] + "'>" + data[i]['course_code'].replace('_', ' ') + " : " + data[i]['course_name'] + "</option> ");
+			}
+			$("#course_name").html(html_for_option);
+		}
+	});
 });
 
-
 $("#course_name").change(function(){
-  let course_code = $("#course_name").val();
-  $.ajax({
-    url : ("/cse_ps/get_sections/"+course_code),
-    method: "GET",
-    success: function(data)
-    {
-      data = JSON.parse(data);
+	let course_code = $("#course_name").val();
+	$.ajax({
+		url : ("/cse_ps/get_sections/"+course_code),
+		method: "GET",
+		success: function(data)
+		{
+			data = JSON.parse(data);
 
-      let html_for_option = "<option value=''>Select Section</option> ";
-      data = data['data'];
+			let html_for_option = "<option value=''>Select Section</option> ";
+			data = data['data'];
 
-      for(let i=0;i<data.length;i++)
-      {
-        html_for_option+=("<option value = '" + data[i]['id'] + "'>" + data[i]['name'] + "</option> ");
-      }
-      $("#section").html(html_for_option);
-    }
-  });
+			for(let i=0;i<data.length;i++)
+			{
+				html_for_option+=("<option value = '" + data[i]['id'] + "'>" + data[i]['name'] + "</option> ");
+			}
+			$("#section").html(html_for_option);
+		}
+	});
 });
 
 $("#check_student").click(function(){
-  let student_id = $("#student_id").val();
-  $.ajax({
-    url : ("/cse_ps/get_student/"+student_id),
-    method: "GET",
-    success: function(data)
-    {
-      data = JSON.parse(data);
-      console.log(data);
-      if(data['message'] != "OK")
-      {
-        $("#student_info").html("No Student Found with student ID " + student_id)
-      }
-      else
-      {
-        let student_information = "";
-        data = data['data'];
+	let student_id_input = $("#student_id");
+	let student_id = $(student_id_input).val();
+	$.ajax({
+		url : ("/cse_ps/get_student/"+student_id),
+		method: "GET",
+		success: function(data)
+		{
+			data = JSON.parse(data);
+			console.log(data);
+			if(data['message'] != "OK")
+			{
+				$(student_id_input).removeClass("valid_project_member");
+				$(student_id_input).addClass("invalid_project_member");
 
-        for(let i=0;i<data.length;i++)
-        {
-          student_information+=(data[i]['name'] + " (" + student_id + "), " + data[i]['department']);
-        }
-        $("#student_info").html(student_information);
-      }
-    }
-  });
+				$("#student_info").html("No Student Found with student ID " + student_id)
+			}
+			else
+			{
+				$(student_id_input).removeClass("invalid_project_member");
+				$(student_id_input).addClass("valid_project_member");
+
+				let student_information = "";
+				data = data['data'];
+
+				for(let i=0;i<data.length;i++)
+				{
+					student_information+=(data[i]['name'] + " (" + student_id + "), " + data[i]['department']);
+				}
+				$("#student_info").html(student_information);
+			}
+		}
+	});
 });
 
 $("#add_member").click(function(){
-  let student_id = $("#student_id").val();
-  $.ajax({
-    url : ("/cse_ps/get_student/"+student_id),
-    method: "GET",
-    success: function(data)
-    {
-      data = JSON.parse(data);
-      console.log(data);
-      if(data['message'] != "OK")
-      {
-        $("#student_info").html("No Student Found with student ID " + student_id)
-      }
-      else
-      {
-        let project_members = $("#project_members").val();
-        if(project_members.length==0)
-        {
-          $("#project_members").html(student_id);
-        }
-        else
-        {
-          $("#project_members").append("," + student_id);
-        }
-      }
-    }
-  });
+	let student_id = $("#student_id").val();
+	$.ajax({
+		url : ("/cse_ps/get_student/"+student_id),
+		method: "GET",
+		success: function(data)
+		{
+			data = JSON.parse(data);
+			console.log(data);
+			if(data['message'] != "OK")
+			{
+				$("#student_info").html("No Student Found with student ID " + student_id)
+			}
+			else
+			{
+				let project_members = $("#project_members").val();
+				if(project_members.length==0)
+				{
+					$("#project_members").html(student_id);
+				}
+				else
+				{
+					$("#project_members").append("," + student_id);
+				}
+			}
+		}
+	});
 });
