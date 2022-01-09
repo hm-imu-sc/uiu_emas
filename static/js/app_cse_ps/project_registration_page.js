d2bd16a830cc92ec502e471e306430e7b5b59795
hm-imu-sc <hmimusc850@gmail.com>
2022-01-09 13:37:59 +0600

    fix applied on project registration page

--- static/js/app_cse_ps/project_registration_page.js
+++ static/js/app_cse_ps/project_registration_page.js
@@ -1,3 +1,8 @@
+
+let temp = {
+	student_ids: {}
+}
+
 $("#course_name").click(function()
 {
 	let option = $("#course_name option");
@@ -45,66 +50,84 @@ $("#course_name").change(function(){
 	});
 });
 
-$("#check_student").click(function(){
-	let student_id_input = $("#student_id");
-	let student_id = $(student_id_input).val();
-	$.ajax({
-		url : ("/cse_ps/get_student/"+student_id),
-		method: "GET",
-		success: function(data)
-		{
-			data = JSON.parse(data);
-			console.log(data);
-			if(data['message'] != "OK")
-			{
-				$(student_id_input).removeClass("valid_project_member");
-				$(student_id_input).addClass("invalid_project_member");
+// $("#check_student").click(function(){
+// 	let student_id_input = $("#student_id");
+// 	let student_id = $(student_id_input).val();
+// 	$.ajax({
+// 		url : ("/cse_ps/get_student/"+student_id),
+// 		method: "GET",
+// 		success: function(data)
+// 		{
+// 			data = JSON.parse(data);
+// 			console.log(data);
+// 			if(data['message'] != "OK")
+// 			{
+// 				$(student_id_input).removeClass("valid_project_member");
+// 				$(student_id_input).addClass("invalid_project_member");
 
-				$("#student_info").html("No Student Found with student ID " + student_id)
-			}
-			else
-			{
-				$(student_id_input).removeClass("invalid_project_member");
-				$(student_id_input).addClass("valid_project_member");
+// 				$("#student_info").html("No Student Found with student ID " + student_id)
+// 			}
+// 			else
+// 			{
+// 				$(student_id_input).removeClass("invalid_project_member");
+// 				$(student_id_input).addClass("valid_project_member");
 
-				let student_information = "";
-				data = data['data'];
+// 				let student_information = "";
+// 				data = data['data'];
 
-				for(let i=0;i<data.length;i++)
-				{
-					student_information+=(data[i]['name'] + " (" + student_id + "), " + data[i]['department']);
-				}
-				$("#student_info").html(student_information);
-			}
-		}
-	});
-});
+// 				for(let i=0;i<data.length;i++)
+// 				{
+// 					student_information+=(data[i]['name'] + " (" + student_id + "), " + data[i]['department']);
+// 				}
+// 				$("#student_info").html(student_information);
+// 			}
+// 		}
+// 	});
+// });
 
 $("#add_member").click(function(){
 	let student_id = $("#student_id").val();
-	$.ajax({
-		url : ("/cse_ps/get_student/"+student_id),
-		method: "GET",
-		success: function(data)
-		{
-			data = JSON.parse(data);
-			console.log(data);
-			if(data['message'] != "OK")
-			{
-				$("#student_info").html("No Student Found with student ID " + student_id)
-			}
-			else
-			{
-				let project_members = $("#project_members").val();
-				if(project_members.length==0)
-				{
-					$("#project_members").html(student_id);
-				}
-				else
-				{
-					$("#project_members").append("," + student_id);
-				}
-			}
-		}
-	});
+	
+	if (temp[student_id]) {
+		return;
+	}
+
+	let project_members = $("#project_members").val();
+	if (project_members.length==0) {
+		$("#project_members").html(student_id);
+	}
+	else {
+		$("#project_members").append("," + student_id);
+	}
+
+	temp[student_id] = true;
+
+	return;
+});
+
+$("#student_id").keyup(function(){
+    let student_id_input = $("#add_member");
+    let student_id = $(this).val();
+
+    if (student_id === "") {
+        return;
+    }
+
+    $.ajax({
+        url : ("/cse_ps/get_student/"+student_id),
+        method: "GET",
+        success: function(data) {
+            data = JSON.parse(data);
+            if (data['message'] != "OK") {
+                $(student_id_input).attr("disabled", "true");
+                $(student_id_input).removeClass("add_anabled");
+                $(student_id_input).addClass("add_disabled");
+            }
+            else {
+                $(student_id_input).removeAttr("disabled");
+                $(student_id_input).removeClass("add_disabled");
+                $(student_id_input).addClass("add_anabled");
+            }
+        }
+    });
 });
